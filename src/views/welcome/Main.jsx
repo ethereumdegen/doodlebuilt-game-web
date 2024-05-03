7c89d965c8a84a1b906fe782a858e072db837c2a
Admazzola <admazzola@gmail.com>
2024-05-03 10:56:33 -0400

    init

---
+++ src/views/welcome/Main.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+ 
+import { observer } from "mobx-react";
+ 
+function Home() {
+
+ 
+
+
+
+
+  return (
+    <div className="flex flex-col min-h-screen overflow-hidden">
+ 
+      {/*  Page content */}
+      <main className="flex-grow">
+
+        {/*  Page sections */}
+         Content 
+
+        {/*<FeatureCode />*/}
+
+      <div>
+ 
+        
+ 
+
+      </div>
+
+
+      </main>
+ 
+
+     
+
+    </div>
+  );
+}
+
+export default observer(Home);
